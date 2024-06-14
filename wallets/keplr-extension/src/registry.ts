1383ef87e0db1cd2448cb50f92d641602c3403a5
Lucas Jiang <secretjiang@gmail.com>
2024-06-14 13:01:51 +0800

    implement keplr wallet

---
+++ wallets/keplr-extension/src/registry.ts
@@ -0,0 +1,26 @@
+
+import { Wallet } from '@interchain-kit/core';
+import { ICON } from './constant';
+
+export const keplrExtensionInfo: Wallet = {
+  windowKey: 'keplr',
+  name: 'keplr-extension',
+  prettyName: 'Keplr',
+  logo: ICON,
+  keystoreChange: 'keplr_keystorechange',
+  downloads: [
+    {
+      device: 'desktop',
+      browser: 'chrome',
+      link: 'https://chrome.google.com/webstore/detail/keplr/dmkamcknogkgcdfhhbddcghachkejeap?hl=en',
+    },
+    {
+      device: 'desktop',
+      browser: 'firefox',
+      link: 'https://addons.mozilla.org/en-US/firefox/addon/keplr/',
+    },
+    {
+      link: 'https://www.keplr.app/download',
+    },
+  ],
+};
