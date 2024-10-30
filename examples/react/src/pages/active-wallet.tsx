a6280235c1d6cbabc14bdc7d7173a3af8b7ef7bf
Lucas Jiang <secretjiang@gmail.com>
2024-10-30 19:53:22 +0800

    update latest interchainjs example

--- examples/react/src/pages/active-wallet.tsx
+++ examples/react/src/pages/active-wallet.tsx
@@ -1,7 +1,7 @@
 import { useChain, useWalletModal } from "@interchain-kit/react";
 
 export default function ActiveWallet() {
-  const { address, wallet, logoUrl } = useChain("juno");
+  const { address, wallet, logoUrl, connect, disconnect } = useChain("juno");
   const { open } = useWalletModal();
   return (
     <>
@@ -12,6 +12,8 @@ export default function ActiveWallet() {
       <p>{address}</p>
       <img src={wallet?.info?.logo?.toString()} style={{ width: "100px" }} />
       <img src={logoUrl} style={{ width: "100px" }}></img>
+      <button onClick={connect}>Connect</button>
+      <button onClick={disconnect}>Disconnect</button>
     </>
   );
 }
