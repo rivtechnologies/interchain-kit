c0d029502b7ec57985377142413062efb1140ee5
danny.kehu <hu-ke@hotmail.com>
2024-11-01 11:42:11 +0800

    use currentWallet instead of local wallet

--- packages/vue/src/modal/modal.vue
+++ packages/vue/src/modal/modal.vue
@@ -5,37 +5,37 @@
       :title="title"
       :hasCloseButton="true"
       :hasBackButton="hasBack"
-      @back="_reset"
+      @back="isList=true"
       :closeButtonProps="closeButtonProps"
     />
-    <!----<ConnectModalQrcode v-if="hasBack" v-bind="qrCodeProps" />-->
+    <ConnectModalQrcode v-if="currentWallet?.info?.mode === 'wallet-connect'" v-bind="qrCodeProps" />
     <ConnectModalWalletList
-      v-if="connectStatus === WalletState.Disconnected"
+      v-else-if="isList"
       :wallets="wallets"
       @wallet-item-click="walletClick"
     />
     <ConnectModalStatus 
       v-else
       :wallet="{
-        name: connectingWallet.info?.name,
-        prettyName: connectingWallet.info?.prettyName,
-        logo: connectingWallet.info?.logo as string,
+        name: currentWallet.info?.name,
+        prettyName: currentWallet.info?.prettyName,
+        logo: currentWallet.info?.logo as string,
         mobileDisable: true
       }"
-      :connected-info="connectedInfo"
-      :status="connectStatus"
+      :connected-info="{}"
+      :status="currentWallet?.walletState"
       :content-header="contentHeader"
       :content-desc="contentDesc"
-      @connect="walletClick(connectingWallet)"
-      @disconnect="disconnect(connectingWallet)"
+      @connect="walletClick(currentWallet)"
+      @disconnect="disconnect(currentWallet)"
     />
   </Modal>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watch, computed } from 'vue'
-import { useWalletManager } from '../composables'
-import { WalletState, WalletAccount } from '@interchain-kit/core';
+import { ref, onMounted, watch, computed, reactive } from 'vue'
+import { useCurrentWallet, useWalletManager } from '../composables'
+import { WalletState, } from '@interchain-kit/core';
 import {
   Box,
   Modal,
@@ -47,26 +47,27 @@ import {
   ConnectModalStatus
 } from "@interchain-ui/vue";
 
-const qrCodeProps = {
-  status: "Done",
-  link: "wc:43529f434fbce35ebc8df08786f70e78f652b7ad45ba1834e5914d8b0dd1e96d@2?relay-protocol=irn&symKey=056b788f4ba347fa65ed0cad68baae3bca1bb5c22d3efc225c7af9e08ad9ddd2",
-  description: "Open Keplr Mobile App to Scan",
-  onRefresh: "ƒ onRefresh() {}",
-  qrCodeSize: 230,
-};
-
 const visible = ref(false);
 const wallets = ref({
 })
-const connectStatus = ref(WalletState.Disconnected)
-// Connecting
-const connectingWallet = ref<any>(null)
+const isList = ref(true)
 // Rejected
 const errorMessage = ref('')
-// Connected
-const account = ref()
-
 const walletManager = useWalletManager();
+const currentWallet = useCurrentWallet();
+
+console.log('cw>', currentWallet)
+watch(currentWallet, (cw) => {
+  console.log('cw', cw)
+})
+
+const qrCodeProps = reactive({
+  status: "Pending",
+  link: "",
+  description: "Open App to connect",
+  onRefresh: () => walletManager.connect(currentWallet.value?.info?.name),
+  qrCodeSize: 230,
+});
 
 onMounted(() => {
   const res = walletManager.wallets.map((w) => {
@@ -83,51 +84,30 @@ onMounted(() => {
 })
 
 const title = computed(() => {
-  if (connectStatus.value === WalletState.Disconnected) {
+  if (!currentWallet.value) {
     return 'Select your wallet'
   } else if (
-    [WalletState.Connecting, WalletState.Rejected, WalletState.Connected].includes(connectStatus.value)
-    && connectingWallet.value) {
-    return connectingWallet.value.info?.prettyName
+    [WalletState.Connecting, WalletState.Rejected, WalletState.Connected, WalletState.Disconnected].includes(currentWallet.value?.walletState)) {
+    return currentWallet.value?.info?.prettyName
   }
 })
 const contentHeader = computed(() => {
-  if (connectStatus.value === WalletState.Connecting) {
+  if (currentWallet.value?.walletState === WalletState.Connecting) {
     return 'Requesting Connection'
-  } else if (connectStatus.value === WalletState.Rejected) {
+  } else if (currentWallet.value?.walletState === WalletState.Rejected) {
     return 'Request Rejected'
   }
 })
 const contentDesc = computed(() => {
-  if (connectStatus.value === WalletState.Connecting) {
-    return `Open the ${connectingWallet.value?.info?.prettyName} browser extension to connect your wallet.`
-  } else if (connectStatus.value === WalletState.Rejected) {
+  if (currentWallet.value?.walletState === WalletState.Connecting) {
+    return `Open the ${currentWallet.value?.info?.prettyName} browser extension to connect your wallet.`
+  } else if (currentWallet.value?.walletState === WalletState.Rejected) {
     return errorMessage.value || 'Connection permission is denied.'
   }
 })
 const hasBack = computed(() => {
-  return connectStatus.value !== WalletState.Disconnected
+  return !isList.value
 })
-
-const connectedInfo = computed(() => {
-  if (account.value) {
-    return  {
-      avatar : "https://picsum.photos/500",
-      name : account.value.username,
-      address : account.value.address
-    }
-  }
-  return {}
-})
-
-const setAccount = async() => {
-  if (connectStatus.value === WalletState.Connected && connectingWallet.value) {
-    const chain = walletManager.chains[0]
-    const act = await connectingWallet.value.getAccount(chain.chainId) as WalletAccount
-    console.log('act', act)
-    account.value = act
-  }
-}
 
 const open = () => {
   visible.value = true;
@@ -136,26 +116,24 @@ const open = () => {
 const close = () => {
   visible.value = false;
   // reset
-  _reset()
+  isList.value = true
 };
 
 const closeButtonProps = {
   onClick: close
 }
 
+watch(walletManager, (wm) => {
+  
+});
+
 const walletClick = async(wallet: any) => {
-  connectStatus.value = WalletState.Connecting
-  connectingWallet.value = wallet
+  isList.value = false
   try {
     await walletManager.connect(wallet?.info.name)
     close()
-    // connectStatus.value = WalletState.Connected
-    // errorMessage.value = ''
-    // setAccount()
   } catch(e: any) {
-    connectStatus.value = WalletState.Rejected
     console.error('[wallet connecting error]', e.message)
-    errorMessage.value = e.message
   }
 }
 
@@ -166,11 +144,6 @@ const disconnect = async(wallet: any) => {
   } catch(e: any) {
     console.log('[wallet disconnecting error]', e.message)
   }
-}
-
-const _reset = () => {
-  connectStatus.value = WalletState.Disconnected
-  connectingWallet.value = null
 }
 
 defineExpose({
