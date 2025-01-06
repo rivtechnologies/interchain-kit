1dbbd793e721c954547d69566f772576428dcddc
Lucas Jiang <secretjiang@gmail.com>
2025-01-06 17:07:34 +0800

    fix vscode can not autocomplete when hot reload

--- scripts/dev.js
+++ scripts/dev.js
@@ -20,7 +20,7 @@ needToInjectDevCommandPackages.forEach((packagePath) => {
     packageJson.scripts = {};
   }
 
-  packageJson.scripts.dev = `tsc -w --preserveWatchOutput -p tsconfig.esm.json`;
+  packageJson.scripts.dev = `tsc -w --preserveWatchOutput`;
 
   fs.writeFileSync(
     packageJsonPath,
