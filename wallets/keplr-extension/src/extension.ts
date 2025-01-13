5084d6bc998e62c015318df0d9a15b793f8346b0
Lucas Jiang <secretjiang@gmail.com>
2025-01-13 12:47:37 +0800

    bump

--- wallets/keplr-extension/src/extension.ts
+++ wallets/keplr-extension/src/extension.ts
@@ -2,8 +2,8 @@
 import { ExtensionWallet } from '@interchain-kit/core'
 
 export class KeplrWallet extends ExtensionWallet {
-    // async init() {
-    //     this.client = await Keplr.getKeplr()
-    //     return this.client
-    // }
+  // async init() {
+  //     this.client = await Keplr.getKeplr()
+  //     return this.client
+  // }
 }
