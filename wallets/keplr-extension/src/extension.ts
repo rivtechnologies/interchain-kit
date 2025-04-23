ad98ea97f77ba49c4dc27430f6991b8726973ff5
Lucas Jiang <secretjiang@gmail.com>
2025-04-23 17:29:31 +0800

    1. add metamask icon 2. add setSignOptions for keplr wallet

--- wallets/keplr-extension/src/extension.ts
+++ wallets/keplr-extension/src/extension.ts
@@ -1,9 +1,1 @@
 
-import { ExtensionWallet } from '@interchain-kit/core'
-
-export class KeplrWallet extends ExtensionWallet {
-  // async init() {
-  //     this.client = await Keplr.getKeplr()
-  //     return this.client
-  // }
-}
--- wallets/keplr-extension/src/index.ts
+++ wallets/keplr-extension/src/index.ts
@@ -1,4 +1,4 @@
-import { CosmosWallet, EthereumWallet, ExtensionWallet, PlatformWallet, WCMobileWebWallet } from "@interchain-kit/core";
+import { CosmosWallet, EthereumWallet, ExtensionWallet, PlatformWallet, SignOptions, WCMobileWebWallet } from "@interchain-kit/core";
 import { keplrExtensionInfo } from "./registry";
 
 export * from './registry'
@@ -7,7 +7,18 @@ const web = new ExtensionWallet(keplrExtensionInfo)
 web.setNetworkWallet('cosmos', new CosmosWallet(keplrExtensionInfo))
 web.setNetworkWallet('eip155', new EthereumWallet(keplrExtensionInfo))
 
-const keplrWallet = new PlatformWallet(keplrExtensionInfo)
+class KeplrPlatformWallet extends PlatformWallet {
+  setSignOptions(options: SignOptions) {
+    const keplrWalletInWeb = keplrWallet.platformWalletMap.get("web") as ExtensionWallet;
+
+    const keplrCosmosWalletInWeb = keplrWalletInWeb.getWalletByChainType("cosmos") as CosmosWallet;
+
+    keplrCosmosWalletInWeb.setSignOptions(options);
+  }
+}
+
+const keplrWallet = new KeplrPlatformWallet(keplrExtensionInfo)
+
 keplrWallet.setPlatformWallet('web', web)
 keplrWallet.setPlatformWallet('mobile-web', new WCMobileWebWallet(keplrExtensionInfo))
 
