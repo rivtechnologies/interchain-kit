5b5426d3d751281396dc47143d674a6be10b6864
Lucas Jiang <secretjiang@gmail.com>
2025-06-17 20:38:55 +0800

    post css build css file

---
+++ packages/react/postcss.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const projectRoot = path.resolve(__dirname, "../../");
+module.exports = {
+  map: false,
+  plugins: [
+    require("postcss-import")({
+      path: [path.resolve(projectRoot, "node_modules")],
+    }),
+    // 可以添加其他 PostCSS 插件，如 autoprefixer 等
+  ],
+};
